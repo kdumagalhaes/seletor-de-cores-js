361cd3bd6428c4b79f5e2333383d4f57f3194c2f
kdumagalhaes <ce.magalhaespereira@gmail.com>
2020-05-20 14:22:06 -0300

    refatoração

--- js/script.js
+++ js/script.js
@@ -11,24 +11,23 @@ function start() {
 function showValueRed() {
     let redSelector = document.querySelector('#redSelector')
     document.getElementById("valueRed").value = redSelector.value
-    document.getElementById("colorResult").style.backgroundColor = 'rgb(' + redSelector.value + ',' + greenSelector.value + ',' + blueSelector.value + ')'
+    showColorResult()
 }
 
 function showValueGreen() {
     let greenSelector = document.querySelector('#greenSelector')
     document.getElementById("valueGreen").value = greenSelector.value
-    document.getElementById("colorResult").style.backgroundColor = 'rgb(' + redSelector.value + ',' + greenSelector.value + ',' + blueSelector.value + ')'
-
+    showColorResult()
 }
 
 function showValueBlue() {
     let blueSelector = document.querySelector('#blueSelector')
     document.getElementById("valueBlue").value = blueSelector.value
-    document.getElementById("colorResult").style.backgroundColor = 'rgb(' + redSelector.value + ',' + greenSelector.value + ',' + blueSelector.value + ')'
-
+    showColorResult()
 }
 
 function showColorResult() {
-    document.getElementById("colorResult").style.backgroundColor = 'rgb(' + redSelector.value + ',' + greenSelector.value + ',' + blueSelector.value + ')'
+    document.getElementById("colorResult").style.backgroundColor = `rgb(${redSelector.value}, ${greenSelector.value}, ${blueSelector.value})`
 }
+
 
